32e1cbcae71dc02d59d991eb6417d652970aefdd
Steerpike <imthekoul@gmail.com>
2022-06-24 02:14:10 +0300

    Firebase config

---
+++ src/Firebase/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDb1EWeduCq_Mwp0XMAriCEniodWy7S5XY',
+  authDomain: 'heron-messaging.firebaseapp.com',
+  projectId: 'heron-messaging',
+  storageBucket: 'heron-messaging.appspot.com',
+  messagingSenderId: '782755353735',
+  appId: '1:782755353735:web:d705007e641633005fd422',
+};
+
+const app = initializeApp(firebaseConfig);
