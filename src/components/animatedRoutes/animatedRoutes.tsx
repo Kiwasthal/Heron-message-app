515a78d5d54e9d3129eb3638d53e7f51a53eb807
Steerpike <imthekoul@gmail.com>
2022-06-25 03:57:37 +0300

    Route change on userLogin

--- src/components/animatedRoutes/animatedRoutes.tsx
+++ src/components/animatedRoutes/animatedRoutes.tsx
@@ -2,14 +2,19 @@ import { Routes, Route, useLocation } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
 import LandingView from '../../views/landingView/landingView';
 import HeronMain from '../../views/heronMain/heronMain';
+import { Navigate } from 'react-router-dom';
+import { useAppSelector } from '../../app/hooks';
 
 const AnimatedRoutes = () => {
+  const user = useAppSelector(state => state.user.userName);
   const location = useLocation();
   return (
     <AnimatePresence exitBeforeEnter>
+      {user && <Navigate to="/heron" replace={true} />}
       <Routes location={location} key={location.pathname}>
         <Route path="/" element={<LandingView></LandingView>} />
         <Route path="/heron" element={<HeronMain />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </AnimatePresence>
   );
