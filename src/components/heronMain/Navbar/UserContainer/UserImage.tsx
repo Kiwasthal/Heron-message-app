84609fc2e0e1c30d0f7845c288235cc4812ded88
Steerpike <imthekoul@gmail.com>
2022-06-26 22:06:27 +0300

    PlaceHolder image added

--- src/components/heronMain/Navbar/UserContainer/UserImage.tsx
+++ src/components/heronMain/Navbar/UserContainer/UserImage.tsx
@@ -1,8 +1,9 @@
 import { StyledUserImage } from '../../../../styledComponents/heronMain/nav/userContainer/styledUserImage';
 import { useAppSelector } from '../../../../app/hooks';
+import userImgPlaceHolder from '../../../../assets/userPlaceHolder.svg';
 
 export const UserImage = () => {
   let userImageSrc = useAppSelector(state => state.user.userImage);
-  if (!userImageSrc) userImageSrc = '';
+  if (!userImageSrc) userImageSrc = userImgPlaceHolder;
   return <StyledUserImage src={userImageSrc} />;
 };
