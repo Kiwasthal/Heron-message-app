5120f4030daf9351f816c4b49e3ba983f530fc8a
Steerpike <imthekoul@gmail.com>
2022-06-27 02:05:17 +0300

    Dispatching logIn methods

--- src/components/landing/buttons/SignInButton.tsx
+++ src/components/landing/buttons/SignInButton.tsx
@@ -2,18 +2,32 @@ import {
   dropIn,
   StyledSignInButton,
 } from '../../../styledComponents/landingPage/buttons/signInButton';
+import { logIn } from '../../../features/user/manualSlice';
+import { useAppDispatch, useAppSelector } from '../../../app/hooks';
 
 export type SignInButtonProps = {
   children: string;
 };
 
 export const SignInButton = ({ children }: SignInButtonProps) => {
+  const userLogInMail = useAppSelector(state => state.manual.userEmailInput);
+  const userLogInPassword = useAppSelector(
+    state => state.manual.userPasswordInput
+  );
+  const dispatch = useAppDispatch();
+  const logInData = {
+    email: userLogInMail,
+    password: userLogInPassword,
+  };
+  const submitLogIn = () => dispatch(logIn(logInData));
+
   return (
     <StyledSignInButton
       variants={dropIn}
       initial="hidden"
       animate="visible"
       exit="exit"
+      onClick={submitLogIn}
     >
       {children}
     </StyledSignInButton>
