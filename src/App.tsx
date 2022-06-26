0ba768468f8e08ae27d4b8d626108a61266c5973
Steerpike <imthekoul@gmail.com>
2022-06-27 02:10:26 +0300

    Styled components global theme added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import { BrowserRouter as Router } from 'react-router-dom';
 import AnimatedRoutes from './components/animatedRoutes/animatedRoutes';
 import './App.css';
+import { ThemeProvider } from 'styled-components';
+import { defaultTheme } from './theme/defaultTheme';
+import { useState } from 'react';
 
 function App() {
+  const [theme, setDefaultTheme] = useState(defaultTheme);
   return (
-    <Router>
-      <AnimatedRoutes />
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <AnimatedRoutes />
+      </Router>
+    </ThemeProvider>
   );
 }
 
