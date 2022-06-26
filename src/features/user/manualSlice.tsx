5dd115f10d2fedd069b8c947afb3b27b7f1f0ae2
Steerpike <imthekoul@gmail.com>
2022-06-27 02:03:54 +0300

    LogIn and signIn methods update

--- src/features/user/manualSlice.tsx
+++ src/features/user/manualSlice.tsx
@@ -1,12 +1,12 @@
 import { auth } from '../../firebase/firebase';
-import firebase from 'firebase/app';
-import { createUserWithEmailAndPassword, UserCredential } from 'firebase/auth';
 import {
-  createSlice,
-  createAsyncThunk,
-  PayloadAction,
-  createAction,
-} from '@reduxjs/toolkit';
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  updateProfile,
+} from 'firebase/auth';
+import { createSlice, createAsyncThunk, PayloadAction } from '@reduxjs/toolkit';
+import { db } from '../../firebase/firebase';
+import { addDoc, collection } from 'firebase/firestore';
 
 type InitialState = {
   userNameInput: string;
@@ -22,15 +22,55 @@ const initialState: InitialState = {
   userConfirmPassword: '',
 };
 
-type UserSubmitProps = {
+type UserSignUpProps = {
+  email: string;
+  password: string;
+  name: string;
+};
+
+type UserLogInProps = {
   email: string;
   password: string;
 };
 
+type CatalogueUserProps = {
+  email: undefined | null | string;
+  name: undefined | null | string;
+};
+
 export const signUp = createAsyncThunk(
   'user/signUp',
-  async ({ email, password }: UserSubmitProps) => {
-    await createUserWithEmailAndPassword(auth, email, password);
+  async ({ email, password, name }: UserSignUpProps) => {
+    let updateDisplayName = {
+      displayName: name,
+    };
+    try {
+      await createUserWithEmailAndPassword(auth, email, password);
+      if (auth.currentUser !== null)
+        await updateProfile(auth.currentUser, updateDisplayName).catch(err =>
+          console.log(err)
+        );
+    } catch (err) {
+      return 'Something went wrong';
+    }
+  }
+);
+
+export const logIn = createAsyncThunk(
+  'user/logIn',
+  async ({ email, password }: UserLogInProps) => {
+    await signInWithEmailAndPassword(auth, email, password);
+  }
+);
+
+export const catalogueUser = createAsyncThunk(
+  'user/catalogue',
+  async ({ email, name }: CatalogueUserProps) => {
+    const userCollection = collection(db, 'users');
+    await addDoc(userCollection, {
+      name: name,
+      email: email,
+    });
   }
 );
 
