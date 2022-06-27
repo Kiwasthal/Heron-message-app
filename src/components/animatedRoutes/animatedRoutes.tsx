1c532124b8396bfdca82da8106198c4a96c05fb5
Steerpike <imthekoul@gmail.com>
2022-06-27 03:21:33 +0300

    Protected Routing

--- src/components/animatedRoutes/animatedRoutes.tsx
+++ src/components/animatedRoutes/animatedRoutes.tsx
@@ -5,19 +5,27 @@ import HeronMainView from '../../views/heronMain/heronMainView';
 import { useNavigate, Navigate } from 'react-router-dom';
 import { useAppSelector } from '../../app/hooks';
 import { useEffect } from 'react';
+import ProtectedRoute from '../privateRoute/privateRoute';
 
 const AnimatedRoutes = () => {
   const navigate = useNavigate();
   const user = useAppSelector(state => state.user.userName);
+  const location = useLocation();
   useEffect(() => {
     if (user) navigate('/heron');
   }, [user]);
-  const location = useLocation();
   return (
     <AnimatePresence exitBeforeEnter>
       <Routes location={location} key={location.pathname}>
         <Route path="/" element={<LandingView />} />
-        <Route path="/heron" element={<HeronMainView />} />
+        <Route
+          path="/heron"
+          element={
+            <ProtectedRoute user={user}>
+              <HeronMainView />
+            </ProtectedRoute>
+          }
+        />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </AnimatePresence>
