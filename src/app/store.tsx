f1b35f16c309049b28418199cfd93088d3164e3b
Steerpike <imthekoul@gmail.com>
2022-06-27 22:08:14 +0300

    Store update

--- src/app/store.tsx
+++ src/app/store.tsx
@@ -1,11 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
 import manualSlice from '../features/user/manualSlice';
 import userSlice from '../features/user/userSlice';
+import navSlice from '../features/nav/navSlice';
+import globalSlice from '../features/messanger/globalSlice';
 
 const store = configureStore({
   reducer: {
     user: userSlice,
     manual: manualSlice,
+    nav: navSlice,
+    global: globalSlice,
   },
 });
 
