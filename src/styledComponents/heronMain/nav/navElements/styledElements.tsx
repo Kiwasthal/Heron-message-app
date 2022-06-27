783cf6a8210b4d8cd93c1f6545a4faf9ca666f13
Steerpike <imthekoul@gmail.com>
2022-06-27 05:44:56 +0300

    Navbar elements

---
+++ src/styledComponents/heronMain/nav/navElements/styledElements.tsx
@@ -0,0 +1,50 @@
+import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { motion } from 'framer-motion';
+
+export const StyledNavElement = styled(motion.div)`
+  display: grid;
+  height: 100%;
+  grid-template-rows: 50% 50%;
+  justify-items: center;
+  border-right: 2px solid #fff;
+  cursor: pointer;
+  background-color: inherit;
+  transition: all 400ms ease;
+  &:hover {
+    background-color: grey;
+  }
+`;
+
+export const LeftMostNavElement = styled(StyledNavElement)`
+  border-bottom-left-radius: 40px;
+`;
+
+export const RightMostNavElement = styled(StyledNavElement)`
+  border-bottom-right-radius: 40px;
+  border-right: none;
+`;
+
+export const ElementTag = styled.p`
+  color: #fff;
+  font-size: 24px;
+  margin: 0;
+  padding: 5px;
+  text-align: center;
+  display: flex;
+  align-items: center;
+  font-weight: 700;
+`;
+
+export const UserIcon = styled.img`
+  height: 50px;
+  width: 50px;
+  justify-self: center;
+  border-radius: 50px;
+`;
+
+export const NavFontIcon = styled(FontAwesomeIcon)`
+  align-self: center;
+  height: 2.25rem;
+  color: #fff;
+`;
