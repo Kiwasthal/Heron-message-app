8295a632426bdb00fea9b62a0745185546516748
Steerpike <imthekoul@gmail.com>
2022-06-29 05:52:51 +0300

    Nav Handling update

--- src/features/nav/navSlice.tsx
+++ src/features/nav/navSlice.tsx
@@ -2,10 +2,14 @@ import { createSlice } from '@reduxjs/toolkit';
 
 type InitialStateProps = {
   globalChat: boolean;
+  friendsElement: boolean;
+  friendsModal: boolean;
 };
 
 const initialState: InitialStateProps = {
   globalChat: false,
+  friendsElement: false,
+  friendsModal: false,
 };
 
 const navSlice = createSlice({
@@ -15,8 +19,22 @@ const navSlice = createSlice({
     showGlobalChat: state => {
       state.globalChat = !state.globalChat;
     },
+    showFriendsElement: state => {
+      state.friendsElement = !state.friendsElement;
+    },
+    openFriendsModal: state => {
+      state.friendsModal = true;
+    },
+    closeFriendsModal: state => {
+      state.friendsModal = false;
+    },
+    closeAllInterfaces: state => {
+      state.globalChat = false;
+      state.friendsElement = false;
+      state.friendsModal = false;
+    },
   },
 });
 
 export default navSlice.reducer;
-export const { showGlobalChat } = navSlice.actions;
+export const { showGlobalChat, showFriendsElement } = navSlice.actions;
