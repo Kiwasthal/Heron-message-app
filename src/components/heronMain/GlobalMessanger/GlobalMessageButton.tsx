4f93ef7d0e78fca449e65ee71ee5fe381a4ecee6
Steerpike <imthekoul@gmail.com>
2022-06-29 03:16:42 +0300

    Scrolling on new inputs

--- src/components/heronMain/GlobalMessanger/GlobalMessageButton.tsx
+++ src/components/heronMain/GlobalMessanger/GlobalMessageButton.tsx
@@ -1,18 +1,30 @@
-import { useAppSelector } from '../../../app/hooks';
+import { useAppDispatch, useAppSelector } from '../../../app/hooks';
 import { StyledGlobalMessageButton } from '../../../styledComponents/heronMain/globalMessanger/styledGlobalMessanger';
 import { useAddGlobalMessageMutation } from '../../../features/api/firebaseApi';
+import { RefObject, useEffect, useRef } from 'react';
 
-const GlobalMessageButton = () => {
+type ButtonProps = {
+  scroll?: RefObject<HTMLDivElement>;
+};
+
+const GlobalMessageButton = (props: ButtonProps) => {
   const messageText = useAppSelector(state => state.global.globalChatInput);
   const userName = useAppSelector(state => state.user.userName);
   const userImage = useAppSelector(state => state.user.userImage);
   const [addMessage] = useAddGlobalMessageMutation();
+
+  useEffect(() => {});
 
   const messageData = {
     text: messageText,
     name: userName,
     profilePicUrl: userImage,
   };
+
+  useEffect(() => {
+    if (props.scroll != null)
+      props.scroll.current?.scrollIntoView({ behavior: 'smooth' });
+  });
 
   const handleSubmit = async () => {
     if (!messageText) return;
