052e060707a1e777a9aa808d95d7c82a156a69bc
Steerpike <imthekoul@gmail.com>
2022-06-30 06:12:30 +0300

    Friend Adding integration

--- src/components/heronMain/FriendsModal/FriendsModal.tsx
+++ src/components/heronMain/FriendsModal/FriendsModal.tsx
@@ -1,16 +1,55 @@
 import { AnimatePresence } from 'framer-motion';
-import { useAppDispatch, useAppSelector } from '../../../app/hooks';
+import { useState } from 'react';
+import {
+  useAppDispatch,
+  useAppSelector,
+  useFriendRequests,
+} from '../../../app/hooks';
 import { closeFriendsModal } from '../../../features/nav/navSlice';
+import { useSendFriendRequestMutation } from '../../../features/api/firebaseApi';
 import {
   StyledFriendsModal,
+  StyledAddFriendInput,
   dropFmodal,
 } from '../../../styledComponents/heronMain/nav/friendsModal/styledFriendsModal';
 import { CloseFriendsModal } from '../../../styledComponents/heronMain/nav/friendsModal/styledFriendsModal';
 
 const FriendsModal = () => {
+  const [inputValue, setInputValue] = useState('');
+  const userName = useAppSelector(state => state.user.userName);
+  const userEmail = useAppSelector(state => state.user.userEmail);
+  const userFriends = useAppSelector(state => state.private.userFriends);
+  const userAcceptedFriends = useAppSelector(
+    state => state.private.acceptedUserFriends
+  );
+  console.log(userAcceptedFriends);
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setInputValue(e.target.value);
   const isShowing = useAppSelector(state => state.nav.friendsModal);
   const dispatch = useAppDispatch();
   const closeModal = () => dispatch(closeFriendsModal());
+  const [sendFriendRequest, data] = useSendFriendRequestMutation();
+
+  const sendRequest = () => {
+    const requestData = {
+      userName,
+      userEmail,
+      friendEmail: inputValue,
+    };
+    if (userEmail === inputValue) {
+      console.log('bug');
+      console.log(userFriends);
+      return;
+    }
+    sendFriendRequest(requestData);
+    if (data.error === 'failed') {
+      console.log('here');
+    }
+    if (data.data === 'resolved') {
+      console.log('we did it');
+    }
+  };
+
   return (
     <AnimatePresence>
       {isShowing && (
@@ -19,9 +58,11 @@ const FriendsModal = () => {
           initial="hidden"
           animate="visible"
           exit="exit"
-          onClick={closeModal}
         >
-          <CloseFriendsModal>+</CloseFriendsModal>
+          <CloseFriendsModal onClick={closeModal}>+</CloseFriendsModal>
+          <p>Search by email</p>
+          <StyledAddFriendInput value={inputValue} onChange={handleChange} />
+          <button onClick={sendRequest}>Add</button>
         </StyledFriendsModal>
       )}
     </AnimatePresence>
