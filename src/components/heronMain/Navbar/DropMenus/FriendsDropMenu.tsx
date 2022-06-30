330cda0c14da958cd26ab1fbf47d70f13fc3101d
Steerpike <imthekoul@gmail.com>
2022-06-30 06:13:46 +0300

    Friend requests rendering

--- src/components/heronMain/Navbar/DropMenus/FriendsDropMenu.tsx
+++ src/components/heronMain/Navbar/DropMenus/FriendsDropMenu.tsx
@@ -1,29 +1,24 @@
-import { AnimatePresence } from 'framer-motion';
 import { useAppDispatch, useAppSelector } from '../../../../app/hooks';
 import { showFriendsElement } from '../../../../features/nav/navSlice';
 import { StyledDropContainer } from '../../../../styledComponents/heronMain/nav/dropmenus/StyledDropMenus';
 import { AddFriendsElement } from './DropContent';
-import { collection, query, where } from 'firebase/firestore';
-import { db } from '../../../../firebase/firebase';
-import { useCollectionData } from 'react-firebase-hooks/firestore';
+import AddFriendCard from './AddFriendCard';
 
 export const FriendsDropContainer = () => {
   const userEmail = useAppSelector(state => state.user.userEmail);
+  const friendList = useAppSelector(state => state.private.userFriends);
   const dispatch = useAppDispatch();
   const clearDisplay = () => dispatch(showFriendsElement());
-  const q = query(
-    collection(db, `users`),
-    where('__name__', '==', `${userEmail}`)
-  );
-  const [friendRequests] = useCollectionData(q);
-  console.log(friendRequests);
-
   return (
     <StyledDropContainer onMouseLeave={clearDisplay}>
       <AddFriendsElement />
-      {friendRequests &&
-        friendRequests[0].friends?.map(request => {
-          return <div>{request.name}</div>;
+      {friendList &&
+        friendList.length > 0 &&
+        friendList.map(friendRequest => {
+          if (!friendRequest.name) return;
+          return (
+            <AddFriendCard key={friendRequest.name} data={friendRequest} />
+          );
         })}
     </StyledDropContainer>
   );
