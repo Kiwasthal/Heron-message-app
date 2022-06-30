474eea95898b47a394e9dac6ffa0b9de4247a0cb
Steerpike <imthekoul@gmail.com>
2022-06-30 06:15:28 +0300

    Custom hook for redux handling along with firebase RTK/api

--- src/app/hooks.ts
+++ src/app/hooks.ts
@@ -1,34 +1,31 @@
+import { collection, query, where } from 'firebase/firestore';
+import { useEffect } from 'react';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
+import {
+  getUserPendingFriendList,
+  getUserAcceptedFriendList,
+} from '../features/messanger/privateSlice';
+import { db } from '../firebase/firebase';
 import type { RootState, AppDispatch } from './store';
 
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 
-// interface MessageProps {
-//   id?: string;
-//   text: string | null;
-//   name: string | null | undefined;
-//   profilePicUrl: string | null | undefined;
-//   createdAt: Timestamp;
-// }
+export const useFriendRequests = () => {
+  const userEmail = useAppSelector(state => state.user.userEmail);
+  const dispatch = useAppDispatch();
+  const q = query(
+    collection(db, `users`),
+    where('__name__', '==', `${userEmail}`)
+  );
+  const [friendRequests] = useCollectionData(q);
 
-// export const UseMessages = () => {
-//   const dispatch = useAppDispatch();
-//   const loading = useAppSelector(state => state.global.loading);
-
-//   useEffect(() => {
-//     onSnapshot(globalCollectionApi, snapshot => {
-//       dispatch(
-//         getMessageSnapshots(
-//           snapshot.docs.map(
-//             doc => ({ ...doc.data(), id: doc.id } as MessageProps)
-//           )
-//         )
-//       );
-//     });
-//   }, []);
-
-//   useEffect;
-
-//   return [loading, messages];
-// };
+  useEffect(() => {
+    //Passing the friend List array to the store on every update//
+    const friendList = friendRequests;
+    if (friendList) dispatch(getUserPendingFriendList(friendList[0].friends));
+    if (friendList)
+      dispatch(getUserAcceptedFriendList(friendList[0].acceptedFriends));
+  }, [friendRequests]);
+};
