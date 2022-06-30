529f77ee7bc765210b6512d4b0a7ce977808b3d2
Steerpike <imthekoul@gmail.com>
2022-06-30 06:11:31 +0300

    Reworwed with email as collection identifier

--- src/features/messanger/privateSlice.tsx
+++ src/features/messanger/privateSlice.tsx
@@ -1,11 +1,22 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { DocumentData } from 'firebase/firestore';
+
+type FriendArrayProps = {
+  name?: string;
+  email: string;
+  status: boolean;
+};
 
 type InitialStateProps = {
   friendInput: null | string;
+  userFriends: [] | FriendArrayProps[];
+  acceptedUserFriends: [] | FriendArrayProps[];
 };
 
 const initialState: InitialStateProps = {
   friendInput: null,
+  userFriends: [],
+  acceptedUserFriends: [],
 };
 
 const privateSlice = createSlice({
@@ -15,8 +26,18 @@ const privateSlice = createSlice({
     getFriendInput: (state, action: PayloadAction<string>) => {
       state.friendInput = action.payload;
     },
+    getUserPendingFriendList: (state, action) => {
+      state.userFriends = action.payload;
+    },
+    getUserAcceptedFriendList: (state, action) => {
+      state.acceptedUserFriends = action.payload;
+    },
   },
 });
 
 export default privateSlice.reducer;
-export const { getFriendInput } = privateSlice.actions;
+export const {
+  getFriendInput,
+  getUserPendingFriendList,
+  getUserAcceptedFriendList,
+} = privateSlice.actions;
