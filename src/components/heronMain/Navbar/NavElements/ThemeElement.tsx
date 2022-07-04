9cc6909c3055b2b436645ca7a19a97e92761c5f1
Steerpike <imthekoul@gmail.com>
2022-07-05 01:43:04 +0300

    Theme dropdown interactivity

--- src/components/heronMain/Navbar/NavElements/ThemeElement.tsx
+++ src/components/heronMain/Navbar/NavElements/ThemeElement.tsx
@@ -3,16 +3,34 @@ import { faEyedropper } from '@fortawesome/free-solid-svg-icons';
 import { StyledNavElement } from '../../../../styledComponents/heronMain/nav/navElements/styledElements';
 import { ElementTag } from '../../../../styledComponents/heronMain/nav/navElements/styledElements';
 import { AnimationControls } from 'framer-motion';
+import { StyledDropContainer } from '../../../../styledComponents/heronMain/nav/dropmenus/StyledDropMenus';
+import ThemeDropMenu from '../DropMenus/ThemeDropMenu';
+import { useAppDispatch, useAppSelector } from '../../../../app/hooks';
+import {
+  hideThemeDropmenu,
+  showThemeDropmenu,
+} from '../../../../features/nav/navSlice';
 
 type ElementProps = {
   value: AnimationControls;
 };
 
 const ThemeElement = (props: ElementProps) => {
+  const themeActive = useAppSelector(state => state.nav.themeElement);
+  const dispatch = useAppDispatch();
+
+  const themeStarHover = () => dispatch(showThemeDropmenu());
+  const themeStopHover = () => dispatch(hideThemeDropmenu());
   return (
-    <StyledNavElement custom={5} animate={props.value}>
+    <StyledNavElement
+      custom={5}
+      animate={props.value}
+      onMouseEnter={themeStarHover}
+      onMouseLeave={themeStopHover}
+    >
       <NavFontIcon icon={faEyedropper} />
       <ElementTag>Theme</ElementTag>
+      {themeActive && <ThemeDropMenu />}
     </StyledNavElement>
   );
 };
