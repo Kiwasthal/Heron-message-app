a238a6121c0b63fce407dc55d4abae63c824b450
Steerpike <imthekoul@gmail.com>
2022-07-05 01:43:26 +0300

    Error handling interactive messages

--- src/components/landing/inputs/SignInEmailInput.tsx
+++ src/components/landing/inputs/SignInEmailInput.tsx
@@ -1,29 +1,48 @@
 import React from 'react';
 import { useState } from 'react';
-import { useAppDispatch } from '../../../app/hooks';
+import { useAppDispatch, useAppSelector } from '../../../app/hooks';
 import { getEmailInput } from '../../../features/user/manualSlice';
 import {
   StyledInput,
   dropInEmail,
+  StyledInputWrapper,
+  StyledErrorText,
 } from '../../../styledComponents/landingPage/inputs/styledInputs';
 
 export const SignInEmailInput = () => {
   const [userEmail, setUserEmail] = useState<string>('');
+  const [emailMiss, setEmailMiss] = useState<boolean>(false);
+  const errors = useAppSelector(state => state.user.errors);
   const dispatch = useAppDispatch();
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) =>
     setUserEmail(e.currentTarget.value);
-  const pushEmailToStore = () => dispatch(getEmailInput(userEmail));
+  const pushEmailToStore = () => {
+    if (userEmail === '') setEmailMiss(true);
+    else dispatch(getEmailInput(userEmail));
+  };
+  console.log(errors);
   return (
-    <StyledInput
-      variants={dropInEmail}
-      initial="hidden"
-      animate="visible"
-      exit="exit"
-      type="text"
-      placeholder="User Email"
-      value={userEmail}
-      onChange={handleChange}
-      onBlur={pushEmailToStore}
-    />
+    <StyledInputWrapper>
+      <StyledInput
+        variants={dropInEmail}
+        initial="hidden"
+        animate="visible"
+        exit="exit"
+        type="text"
+        placeholder="User Email"
+        value={userEmail}
+        onChange={handleChange}
+        onBlur={pushEmailToStore}
+      />
+      <StyledErrorText>
+        {emailMiss
+          ? 'Please input your email'
+          : errors === 'auth/invalid-email'
+          ? 'Invalid Email'
+          : errors === 'auth/user-not-found'
+          ? "Can't find Email."
+          : null}
+      </StyledErrorText>
+    </StyledInputWrapper>
   );
 };
--- src/components/landing/inputs/SignInPasswordInput.tsx
+++ src/components/landing/inputs/SignInPasswordInput.tsx
@@ -1,28 +1,47 @@
 import {
   StyledInput,
   dropInName,
+  StyledInputWrapper,
+  StyledErrorText,
 } from '../../../styledComponents/landingPage/inputs/styledInputs';
-import { useAppDispatch } from '../../../app/hooks';
+import { useAppDispatch, useAppSelector } from '../../../app/hooks';
 import { useState } from 'react';
 import { getPasswordInput } from '../../../features/user/manualSlice';
 
 export const SignInPasswordInput = () => {
+  const errors = useAppSelector(state => state.user.errors);
+  const [missingPass, setMissingPass] = useState<boolean>(false);
   const [userPassword, setUserPassword] = useState<string>('');
   const dispatch = useAppDispatch();
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) =>
     setUserPassword(e.currentTarget.value);
-  const pushUserNameToStore = () => dispatch(getPasswordInput(userPassword));
+  const pushUserNameToStore = () => {
+    if (userPassword === '') {
+      setMissingPass(true);
+    } else dispatch(getPasswordInput(userPassword));
+  };
   return (
-    <StyledInput
-      variants={dropInName}
-      initial="hidden"
-      animate="visible"
-      exit="exit"
-      type="password"
-      placeholder="Enter you Password"
-      value={userPassword}
-      onChange={handleChange}
-      onBlur={pushUserNameToStore}
-    />
+    <StyledInputWrapper>
+      <StyledInput
+        variants={dropInName}
+        initial="hidden"
+        animate="visible"
+        exit="exit"
+        type="password"
+        placeholder="Enter you Password"
+        value={userPassword}
+        onChange={handleChange}
+        onBlur={pushUserNameToStore}
+      />
+      <StyledErrorText>
+        {missingPass
+          ? 'Please input your password'
+          : errors === 'auth/wrong-password'
+          ? 'Wrong Password'
+          : errors === 'auth/too-many-requests'
+          ? 'Too many wrong attemtps, please try again later.'
+          : null}
+      </StyledErrorText>
+    </StyledInputWrapper>
   );
 };
