a2fcecfe48dc5666a9c0c732e439e57e5c0efd94
Steerpike <imthekoul@gmail.com>
2022-07-08 03:42:23 +0300

    Send message with kedown

--- src/components/heronMain/PrivateMessanger/MessageTextarea.tsx
+++ src/components/heronMain/PrivateMessanger/MessageTextarea.tsx
@@ -1,5 +1,6 @@
-import { Dispatch, SetStateAction, useEffect, useState } from 'react';
-import { useAppSelector } from '../../../app/hooks';
+import React, { Dispatch, SetStateAction, useEffect } from 'react';
+import { useAppDispatch, useAppSelector } from '../../../app/hooks';
+import { useAddPrivateMessageMutation } from '../../../features/api/firebaseApi';
 import { StyledPrivateTextarea } from '../../../styledComponents/heronMain/privateMessanger/styledPrivateMessanger';
 
 type TextAreaProps = {
@@ -8,6 +9,31 @@ type TextAreaProps = {
 };
 
 const MessageTextarea = (props: TextAreaProps) => {
+  const message = props.value;
+  const userName = useAppSelector(state => state.user.userName);
+  const userEmail = useAppSelector(state => state.user.userEmail);
+  const userImage = useAppSelector(state => state.user.userImage);
+  const chatId = useAppSelector(state => state.private.currentChatroom);
+  const dispatch = useAppDispatch();
+  const [addMessage, data] = useAddPrivateMessageMutation();
+
+  const messageData = {
+    chatId: chatId,
+    text: message,
+    name: userName,
+    email: userEmail,
+    profilePicUrl: userImage,
+  };
+
+  const sendMessage = async () => {
+    if (!message) return;
+    await addMessage(messageData);
+  };
+
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    if (event.key === 'Enter') sendMessage();
+  };
+
   const loading = useAppSelector(state => state.private.loading);
   const chatroom = useAppSelector(state => state.private.currentChatroom);
   const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
@@ -15,13 +41,14 @@ const MessageTextarea = (props: TextAreaProps) => {
   };
 
   useEffect(() => {
-    if (loading) props.setValue('');
-  }, [loading]);
+    if (loading || data.status === 'fulfilled') props.setValue('');
+  }, [loading, data.status]);
 
   return (
     <StyledPrivateTextarea
       value={props.value}
       onChange={handleChange}
+      onKeyDown={handleKeyDown}
       disabled={chatroom ? false : true}
     />
   );
